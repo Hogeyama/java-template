5b65f5fba71f0766fd30a940edbdf83e905b29ca
hogeyama <gan13027830@gmail.com>
2024-11-18 00:37:58 +0900

    Don't use Date

--- src/main/java/com/example/demo/security/service/JwtService.java
+++ src/main/java/com/example/demo/security/service/JwtService.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
+import java.time.Instant;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -41,14 +42,15 @@ public class JwtService {
                 .map(role -> role.getName())
                 .collect(Collectors.toList()));
 
+        Instant now = Instant.now();
         return Jwts.builder()
-                .claims(claims)
-                .subject(user.getUsername())
-                .id(UUID.randomUUID().toString()) // JTI
-                .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + expiration * 1000))
-                .signWith(key)
-                .compact();
+            .claims(claims)
+            .subject(user.getUsername())
+            .id(UUID.randomUUID().toString())
+            .issuedAt(Date.from(now))
+            .expiration(Date.from(now.plusSeconds(expiration)))
+            .signWith(key)
+            .compact();
     }
 
     public boolean validateToken(String token) {
