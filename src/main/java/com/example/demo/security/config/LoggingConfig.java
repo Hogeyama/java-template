8f416bea0ff47443d4280e94e044a18ab3c9f7ef
hogeyama <gan13027830@gmail.com>
2024-11-17 22:11:13 +0900

    wip: log

---
+++ src/main/java/com/example/demo/security/config/LoggingConfig.java
@@ -0,0 +1,19 @@
+package com.example.demo.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class LoggingConfig {
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(false);
+        filter.setIncludeHeaders(true);
+        filter.setBeforeMessagePrefix("Request [");
+        filter.setAfterMessagePrefix("Response [");
+        return filter;
+    }
+}
